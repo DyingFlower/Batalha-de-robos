d36bc7530250c397e8103c442e2212db3873c9f9
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-11-02 18:17:01 -0300

    Update Arthur.java

--- Arthur.java
+++ Arthur.java
@@ -1,29 +1,29 @@
 package tecProg;
-import java.awt.Color;
-import robocode.AdvancedRobot;
-import robocode.HitWallEvent;
-import robocode.ScannedRobotEvent;
-public class Arthur extends AdvancedRobot{
-	public void run() {
-		setColors(Color.black, Color.green, Color.pink);
+	import java.awt.Color;
+	import robocode.AdvancedRobot;
+	import robocode.HitWallEvent;
+	import robocode.ScannedRobotEvent;
+	public class Arthur extends AdvancedRobot{
+		public void run() {
+			setColors(Color.black, Color.green, Color.white);
 
-		while (true) {
-			ahead(100);
-			turnLeft(20);
-			turnGunRight(360);
+			while (true) {
+				ahead(1000);
+				turnGunRight(360);
 
+			}
 		}
-	}
-	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(3);
-		fire(3);
-		System.out.println(this.getName() + " - "+e.getDistance());
+		public void onScannedRobot(ScannedRobotEvent e) {
+			while(true) {
+				fire(3);
+			}
+			
+		}
+		public void onHitWall(HitWallEvent event) {
 		
-	}
-	public void onHitWall(HitWallEvent event) {
-	
-        turnLeft(90);
-        //criar while para enquanto não entiver com o robo inimigo na mira.
-	}
+	        turnLeft(90);
+	        turnGunLeft(360);
+		}
 
-}
+	}
+	
